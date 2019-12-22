bac130714383c6020973e8dc00b0d7bb3b51602e
Pranav5000 <pranavpw815@gmail.com>
2019-12-22 17:29:36 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {BrowserRouter} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename='/ImageFinder/'>
+      <BrowserRouter basename='/MStore/'>
         <Navbars/>
         <Route render={({location}) => (
                   <Switch location={location}>
