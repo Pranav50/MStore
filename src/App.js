173598210679a1138c3c2808795434405334f67e
Pranav5000 <pranavpw815@gmail.com>
2019-12-22 17:27:13 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -9,27 +9,19 @@ import Details from './components/Details';
 import ProductList from './components/ProductList';
 import Modal from './components/Modal';
 import {BrowserRouter} from 'react-router-dom'
-import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Navbars/>
-        <Route render={({location}) => ( 
-              <TransitionGroup>
-                <CSSTransition 
-                timeout={280}
-                classNames="fade"
-                key={location.key}>
+        <Route render={({location}) => (
                   <Switch location={location}>
                     <Route path="/details" component={Details}/>
                     <Route path="/cart" component={Cart}/>
                     <Route exact path="/" component={ProductList}/>
                     <Route path="/default" component={Default}/>
                   </Switch>
-                </CSSTransition>
-              </TransitionGroup>
         )} /> 
         <Modal/>
         </BrowserRouter>
