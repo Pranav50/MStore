16d96f0169b3e8116d46f3613f5dfe06cf4f5586
Pranav5000 <pranavpw815@gmail.com>
2019-12-22 17:13:20 +0530

    Updates

---
+++ precache-manifest.1a3c30afe9cf2582dee3e8cda044d1f6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b9e1d17778e5145265",
+    "url": "/MStore/static/css/main.fd683adc.chunk.css"
+  },
+  {
+    "revision": "32b9e1d17778e5145265",
+    "url": "/MStore/static/js/main.454ee8b9.chunk.js"
+  },
+  {
+    "revision": "34b0b002d7a2b20fd514",
+    "url": "/MStore/static/js/runtime~main.c81c7e33.js"
+  },
+  {
+    "revision": "6dff032a737886d507c0",
+    "url": "/MStore/static/css/2.e9156969.chunk.css"
+  },
+  {
+    "revision": "6dff032a737886d507c0",
+    "url": "/MStore/static/js/2.76a268bc.chunk.js"
+  },
+  {
+    "revision": "c7704d100fa692a6406b316f096e55fd",
+    "url": "/MStore/static/media/product-1.c7704d10.png"
+  },
+  {
+    "revision": "0a99080d09d8828332eaffe31186a7db",
+    "url": "/MStore/static/media/samsung.0a99080d.png"
+  },
+  {
+    "revision": "458db6b00b72d7fe161edff4b9e513c9",
+    "url": "/MStore/static/media/product-2.458db6b0.png"
+  },
+  {
+    "revision": "870c62c9d02cf0af645b530024f90514",
+    "url": "/MStore/static/media/product-3.870c62c9.png"
+  },
+  {
+    "revision": "808050d55f281b632309a414119201ed",
+    "url": "/MStore/static/media/sony.808050d5.png"
+  },
+  {
+    "revision": "4d011a4f007849ea2796f20b36166699",
+    "url": "/MStore/static/media/product-4.4d011a4f.png"
+  },
+  {
+    "revision": "8c14b2aae0d9f640d948ecbfc1db05be",
+    "url": "/MStore/static/media/motorola.8c14b2aa.png"
+  },
+  {
+    "revision": "80e1d4d9356324a53a9c1ff641ebb66b",
+    "url": "/MStore/static/media/product-5.80e1d4d9.png"
+  },
+  {
+    "revision": "9c98076a26f192a9ff4f949304f3d20f",
+    "url": "/MStore/static/media/product-6.9c98076a.png"
+  },
+  {
+    "revision": "ede157e6704d114bf564b50d89f6084b",
+    "url": "/MStore/static/media/product-7.ede157e6.png"
+  },
+  {
+    "revision": "5c275726ae5b66e0c60c51d61f4260f3",
+    "url": "/MStore/static/media/product-8.5c275726.png"
+  },
+  {
+    "revision": "342ebb0499b3d2ae2aa83abb63531d86",
+    "url": "/MStore/static/media/oneplus.342ebb04.png"
+  },
+  {
+    "revision": "23b046dd5c15fd07eed09e225734515d",
+    "url": "/MStore/index.html"
+  }
+];
